79c4df96a6cf0f9568176a2841837c4288d08ab2
Umayr Shahid <umayrr@hotmail.co.uk>
2014-10-16 19:08:03 +0500

    Added Coloured ls for ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
+$scripts = "$(split-path $profile)\Scripts"
+$modules = "$(split-path $profile)\Modules"
+$docs    =  $(resolve-path "$Env:userprofile\documents")
+$desktop =  $(resolve-path "$Env:userprofile\desktop")
+
 Set-Alias rc Edit-PowershellProfile
 
 function Prompt
@@ -12,4 +17,42 @@ function Prompt
 function Edit-PowershellProfile
 {
     notepad $Profile
+}
+
+function Write-ColorOutput($ForegroundColor)
+{
+    # save the current color
+    $fc = $host.UI.RawUI.ForegroundColor
+
+    # set the new color
+    $host.UI.RawUI.ForegroundColor = $ForegroundColor
+
+    # output
+    if ($args) {
+        Write-Output $args
+    }
+    else {
+        $input | Write-Output
+    }
+
+    # restore the original color
+    $host.UI.RawUI.ForegroundColor = $fc
+}
+
+function Color-LS
+{
+    dir $args | Format-High -Print {    
+        $item = $args
+        $fore = $host.UI.RawUI.ForegroundColor        
+        $host.UI.RawUI.ForegroundColor = .{     
+            if ($item[1].psIsContainer) {'Blue'}
+            elseif ($item[1].Extension -match '\.(exe|bat|cmd|ps1|psm1|vbs|rb|reg|dll|o|lib)') {'Red'}
+            elseif ($item[1].Extension -match '\.(zip|tar|gz|rar)') {'Yellow'}
+            elseif ($item[1].Extension -match '\.(py|pl|cs|rb|h|cpp)') {'Cyan'}
+            elseif ($item[1].Extension -match '\.(txt|cfg|conf|ini|csv|log|xml)') {'Green'}
+            else {$fore}
+        }
+        write-host $args[0] -NoNewLine
+        $host.UI.RawUI.ForegroundColor = $fore
+    }
 }
